ea5adf928ba3f19b3af7e800263735f0c74172b2
Christopher Shipstone <christopher.shipstone98@gmail.com>
2022-05-13 12:08:15 +0100

    Added ModifiedUtc property

---
+++ Migrations/20220513110722_PostModifiedUtc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shipstone.OpenBook.Migrations
+{
+    public partial class PostModifiedUtc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedUtc",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedUtc",
+                table: "Posts");
+        }
+    }
+}
--- Models/Post.cs
+++ Models/Post.cs
@@ -20,5 +20,13 @@ namespace Shipstone.OpenBook.Models
         public virtual User Creator { get; set; }
         public int CreatorId { get; set; }
         public int Id { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Modified at")]
+        public DateTime Modified => this.ModifiedUtc.ToLocalTime();
+        
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Modified at")]
+        public DateTime ModifiedUtc { get; set; }
     }
 }
