522c5557e1ff0a43ffd4990ca91352faeceae815
Christopher Shipstone <christopher.shipstone98@gmail.com>
2022-05-13 14:19:02 +0100

    Added home page posts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+using Shipstone.OpenBook.Data;
+using Shipstone.OpenBook.Models;
 using Shipstone.OpenBook.ViewModels;
 
 namespace Shipstone.OpenBook.Controllers
@@ -10,13 +14,35 @@ namespace Shipstone.OpenBook.Controllers
     [Controller]
     public class HomeController : Controller
     {
+        private readonly Context _Context;
         private readonly ILogger<HomeController> _Logger;
+        private readonly UserManager<User> _UserManager;
 
-        public HomeController(ILogger<HomeController> logger) =>
+        public HomeController(
+            ILogger<HomeController> logger,
+            Context context,
+            UserManager<User> userManager
+        )
+        {
+            this._Context = context;
             this._Logger = logger;
+            this._UserManager = userManager;
+        }
 
+        [ActionName("Index")]
         [Authorize]
-        public IActionResult Index() => this.View();
+        [HttpGet]
+        public async Task<IActionResult> IndexAsync()
+        {
+            User user = await this._UserManager.GetUserAsync(this.User);
+
+            return this.View(new ProfileViewModel
+            {
+                IsCurrentUser = true,
+                Posts = (await PostDao.RetrieveAllAsync(this._Context, user)).ViewModel,
+                User = user
+            });
+        }
 
         public IActionResult Privacy() => this.View();
 
